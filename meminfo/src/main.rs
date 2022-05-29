1370272253cf028e8ff2352ebcbe3d8fd34b0d86
jyuch <jyuch@users.noreply.github.com>
2022-05-29 20:41:52 +0900

    GlobalMemoryStatusEx

---
+++ meminfo/src/main.rs
@@ -0,0 +1,22 @@
+use windows::Win32::System::SystemInformation::{GlobalMemoryStatusEx, MEMORYSTATUSEX};
+
+fn main() {
+    let status = unsafe {
+        let mut status = MEMORYSTATUSEX {
+            dwLength: std::mem::size_of::<MEMORYSTATUSEX>() as u32,
+            ..Default::default()
+        };
+        GlobalMemoryStatusEx(&mut status);
+
+        status
+    };
+
+    println!("MemoryLoad: {}", status.dwMemoryLoad);
+    println!("TotalPhys: {}", status.ullTotalPhys);
+    println!("AvailPhys: {}", status.ullAvailPhys);
+    println!("TotalPageFile: {}", status.ullTotalPageFile);
+    println!("AvailPageFile: {}", status.ullAvailPageFile);
+    println!("TotalVirtual: {}", status.ullTotalVirtual);
+    println!("AvailVirtual: {}", status.ullAvailVirtual);
+    println!("AvailExtendedVirtual: {}", status.ullAvailExtendedVirtual);
+}
