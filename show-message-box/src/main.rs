79d194dc861815a5fec5c61687ddaa7590abe003
jyuch <jyuch@users.noreply.github.com>
2022-05-30 22:25:52 +0900

    show-message-box を windows クレートに変更

--- show-message-box/src/main.rs
+++ show-message-box/src/main.rs
@@ -1,45 +1,28 @@
-extern crate winapi;
-
+use compat::to_wstring;
 use std::io::Error;
+use windows::core::PCWSTR;
+use windows::Win32::UI::WindowsAndMessaging::{MessageBoxW, MB_ICONINFORMATION, MB_OK};
 
-fn to_wstring(value: &str) -> Vec<u16> {
-    use std::os::windows::ffi::OsStrExt;
-    std::ffi::OsStr::new(value)
-        .encode_wide()
-        .chain(std::iter::once(0))
-        .collect()
-}
-
-#[cfg(windows)]
 fn show_message_box(title: &str, msg: &str) -> Result<i32, Error> {
-    use std::ptr::null_mut;
-    use winapi::um::winuser::{MessageBoxW, MB_ICONINFORMATION, MB_OK};
-
     let msg_w = to_wstring(msg);
     let title_w = to_wstring(title);
     let ret = unsafe {
         MessageBoxW(
-            null_mut(),
-            msg_w.as_ptr(),
-            title_w.as_ptr(),
+            None,
+            PCWSTR(msg_w.as_ptr()),
+            PCWSTR(title_w.as_ptr()),
             MB_OK | MB_ICONINFORMATION,
         )
     };
-    if ret == 0 {
+    if ret.0 == 0 {
         Err(Error::last_os_error())
     } else {
-        Ok(ret)
+        Ok(ret.0)
     }
 }
 
-#[cfg(windows)]
 fn main() -> Result<(), Error> {
     let ret = show_message_box("Greeting from Rust.", "Hello Win32 API.")?;
     println!("{}", ret);
     Ok(())
-}
-
-#[cfg(not(windows))]
-fn main() {
-    println!("This program only work on Windows.")
 }
