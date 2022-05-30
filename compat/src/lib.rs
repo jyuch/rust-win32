d03d6a54cdcaa0be1b075ae43ac27e4625e24fef
jyuch <jyuch@users.noreply.github.com>
2022-05-30 19:44:27 +0900

    enum-printer を windows クレートに変更

---
+++ compat/src/lib.rs
@@ -0,0 +1,36 @@
+use windows::core::PWSTR;
+
+pub fn to_wstring(value: &str) -> Vec<u16> {
+    use std::os::windows::ffi::OsStrExt;
+    std::ffi::OsStr::new(value)
+        .encode_wide()
+        .chain(std::iter::once(0))
+        .collect()
+}
+
+pub unsafe fn pwstr_to_string(ptr: PWSTR) -> String {
+    use std::slice::from_raw_parts;
+    let len = (0usize..)
+        .find(|&n| *ptr.0.offset(n as isize) == 0)
+        .expect("Couldn't find null terminator");
+    let array: &[u16] = from_raw_parts(ptr.0, len);
+    String::from_utf16_lossy(array)
+}
+
+pub fn string_from_pwstr(source: PWSTR) -> Option<String> {
+    if source.0.is_null() {
+        None
+    } else {
+        let mut buffer = Vec::new();
+        let mut pwz = source.0;
+
+        unsafe {
+            while *pwz != 0 {
+                buffer.push(*pwz);
+                pwz = pwz.add(1);
+            }
+        }
+
+        Some(String::from_utf16(&buffer).expect("string_from_pwstr"))
+    }
+}
--- enum-printer/src/main.rs
+++ enum-printer/src/main.rs
@@ -1,85 +1,56 @@
-use crate::compat::pwstr_to_string;
+use compat::{pwstr_to_string, string_from_pwstr};
+use std::alloc::{alloc, dealloc, Layout};
+use std::ffi::c_void;
 use std::io::Error;
-use winapi::_core::ptr::null_mut;
-use winapi::_core::slice::from_raw_parts;
-use winapi::shared::minwindef::{BOOL, DWORD};
-use winapi::um::winspool::{EnumPrintersW, PRINTER_ENUM_LOCAL, PRINTER_INFO_2W};
+use std::ptr::{null, null_mut};
+use std::slice::from_raw_parts;
+use windows::Win32::Foundation::BOOL;
+use windows::Win32::Graphics::Printing::{EnumPrintersW, PRINTER_ENUM_LOCAL, PRINTER_INFO_2W};
+use windows::Win32::System::Memory::{
+    VirtualAlloc, VirtualFree, MEM_COMMIT, MEM_RELEASE, PAGE_READWRITE,
+};
 
-mod compat;
-
-#[derive(Debug)]
-struct Printer {
-    name: String,
-    driver: String,
-}
-
-impl Printer {
-    fn new(name: &str, driver: &str) -> Printer {
-        Self {
-            name: name.to_string(),
-            driver: driver.to_string(),
-        }
-    }
-}
-
-fn get_printers() -> Result<Vec<Printer>, Error> {
-    let mut needed: DWORD = 0 as DWORD;
-    let mut returned: DWORD = 0 as DWORD;
+fn main() {
+    let mut needed: u32 = 0;
+    let mut returned: u32 = 0;
 
     unsafe {
         EnumPrintersW(
             PRINTER_ENUM_LOCAL,
-            null_mut(),
+            None,
             2,
             null_mut(),
             0,
             &mut needed,
             &mut returned,
-        )
-    };
+        );
+    }
 
-    let printer = unsafe { libc::malloc(needed as libc::size_t) as *mut PRINTER_INFO_2W };
+    let layout = Layout::array::<u8>(needed as usize).expect("Failed to create layout.");
 
-    let ret: BOOL = unsafe {
+    unsafe {
+        let printer_enum_pointer = alloc(layout);
         EnumPrintersW(
             PRINTER_ENUM_LOCAL,
-            null_mut(),
+            None,
             2,
-            printer as *mut u8,
+            printer_enum_pointer,
             needed,
             &mut needed,
             &mut returned,
-        )
-    };
+        );
 
-    if ret == 0 {
-        unsafe {
-            libc::free(printer as *mut libc::c_void);
+        let arr: &[PRINTER_INFO_2W] = std::slice::from_raw_parts(
+            printer_enum_pointer as *mut PRINTER_INFO_2W,
+            returned as usize,
+        );
+
+        for it in arr {
+            let name = string_from_pwstr(it.pPrinterName).unwrap();
+            let driver = string_from_pwstr(it.pDriverName).unwrap();
+            println!("Name:{} Driver:{}", name, driver);
         }
-        return Err(Error::last_os_error());
-    }
 
-    let mut printers = Vec::<Printer>::with_capacity(returned as usize);
-    unsafe {
-        let array = from_raw_parts(printer, returned as usize);
-
-        for it in array {
-            let printer_name = pwstr_to_string(it.pPrinterName);
-            let driver_name = pwstr_to_string(it.pDriverName);
-            printers.push(Printer::new(printer_name.as_str(), driver_name.as_str()));
-        }
-    }
-
-    unsafe {
-        libc::free(printer as *mut libc::c_void);
-    }
-
-    Ok(printers)
-}
-
-fn main() {
-    let printers = get_printers().expect("Failed to get printer info");
-    for it in printers {
-        println!("{:?}", it);
+        dealloc(printer_enum_pointer, layout);
     }
 }
