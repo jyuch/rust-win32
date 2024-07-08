e6ec6f190efeef0c781edffd71ba78aa35c81ca4
jyuch <jyuch@users.noreply.github.com>
2024-07-08 20:22:54 +0900

    windows 0.58

---
+++ run.ps1
@@ -0,0 +1,6 @@
+cargo run --bin cpp-interop
+cargo run --bin enum-printer
+cargo run --bin icon-and-property
+cargo run --bin meminfo
+cargo run --bin print-via-gdi
+cargo run --bin show-message-box
